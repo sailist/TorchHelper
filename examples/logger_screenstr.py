b480756cf0c8347c53e805d358d57f85a660c789
sailist <sailist@outlook.com>
2020-01-28 10:09:30 +0800

    添加的打印日志的示例

---
+++ examples/logger_screenstr.py
@@ -0,0 +1,46 @@
+'''
+   Copyright 2020 Sailist
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+    温馨提示：
+        抵制不良代码，拒绝乱用代码。
+        
+        注意自我保护，谨防上当受骗。
+        
+        适当编程益脑，沉迷编程伤身。
+        
+        合理安排时间，享受健康生活！
+'''
+
+import sys
+sys.path.insert(0,"../")
+
+from torchhelper import *
+
+from torchhelper.frame.logwrapper import Logger
+from torchhelper.base.structure import ScreenStr
+ScreenStr.debug = True
+print(VERSION)
+
+logger = Logger()
+import time
+for i in range(1000):
+    logger.inline(prefix=str([i for i in range(30)]))
+    time.sleep(0.01)
+
+logger.line(str([i for i in range(30)]))
+
+for i in range(1000):
+    logger.inline(prefix=str([i for i in range(30)]))
+    time.sleep(0.01)
---
+++ examples/screenstrs.py
@@ -0,0 +1,37 @@
+'''
+   Copyright 2020 Sailist
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+    温馨提示：
+        抵制不良代码，拒绝乱用代码。
+        
+        注意自我保护，谨防上当受骗。
+        
+        适当编程益脑，沉迷编程伤身。
+        
+        合理安排时间，享受健康生活！
+'''
+
+import sys
+sys.path.insert(0,"../")
+
+from torchhelper import *
+
+from torchhelper.base.structure import ScreenStr
+print(VERSION)
+# ScreenStr.debug = True
+import time
+for i in range(10000):
+    print(ScreenStr(ScreenStr.test_str(50)),end="",flush=True)
+    time.sleep(0.01)
